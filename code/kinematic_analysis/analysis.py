b6dc644b71b4c1a84df3dfa5ffaf22b18a6f6021
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-06-06 16:35:16 +0530

    removed spaces

--- code/kinematic_analysis/analysis.py
+++ code/kinematic_analysis/analysis.py
@@ -1,6 +1,4 @@
 from math import *
-
-
 class RobotArm:
     def __init__(self, dimensions):
         self.l0 = dimensions["l0"]
@@ -16,13 +14,11 @@ class RobotArm:
 
     def getGripperPosition(self, q1, q2, q3):
         phi = (q3 + q2) - 90
-
         # Converting angles phi, q1, q2, q3 from degrees to radians
         phi = radians(phi)
         q1 = radians(q1)
         q2 = radians(q2)
         q3 = radians(q3)
-
         f = sqrt((self.l3i**2 + self.l2**2) - (2 * self.l3i * self.l2 * cos(phi)))
         tau = asin((self.l3i * sin(phi)) / f)
         mu = acos((f**2 + self.l3o**2 - self.l4**2) / (2 * f * self.l3o))
@@ -31,7 +27,6 @@ class RobotArm:
         px = self.l0 + ((self.l1 + r + self.l5) * sin(q1)) - (self.d * cos(q1))
         py = -((self.l1 + r + self.l5) * cos(q1)) - (self.d * sin(q1))
         pz = self.h + (self.l2 * sin(q2)) + (self.l3 * sin(q2 + q3o))
-
         print(f"Px = {px}, Py = {py}, Pz = {pz}")  # Printing Gripper Position
 
 
@@ -48,9 +43,6 @@ dimensions = {
     "d": 5,
     "h": 64.5
 }
-
 # Creating a Robot Arm Instance
 myArm = RobotArm(dimensions)
-
-# This line prints the gripper position when q1 = 90, q2 = 90 and q3 = 90
-myArm.getGripperPosition(90, 90, 90)
+myArm.getGripperPosition(90, 90, 90)# This line prints the gripper position when q1 = 90, q2 = 90 and q3 = 90
